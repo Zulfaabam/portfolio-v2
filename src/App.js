1a6c3fe7fa20e0cbb25925b22315b738aa71a713
Zulfaabam <63113235+Zulfaabam@users.noreply.github.com>
2021-10-13 19:42:42 +0700

    Edited file structure

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React, { Component } from 'react'
 import './App.css'
-import Header from './Header'
-import Main from './Main'
-import Footer from './Footer'
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Header />
-
         <Main />
-
         <Footer />
       </div>
     )
