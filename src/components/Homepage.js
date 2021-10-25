f2576cef85ec6f40ade64e2ba5b0a1bb29a9ace7
Zulfaabam <63113235+Zulfaabam@users.noreply.github.com>
2021-10-25 10:07:59 +0700

    edited page transition animation

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -2,14 +2,19 @@ import React from 'react'
 import Hero from './Hero'
 import Main from './Main'
 import Footer from './Footer'
-// import { motion } from 'framer-motion'
+import { motion } from 'framer-motion'
 
 export default function Homepage() {
   return (
-    <div>
+    <motion.div
+      initial={{ y: '100vh' }}
+      animate={{ y: 0 }}
+      exit={{ x: '-100vw' }}
+      transition={{ duration: 0.5 }}
+    >
       <Hero />
       <Main />
       <Footer />
-    </div>
+    </motion.div>
   )
 }
