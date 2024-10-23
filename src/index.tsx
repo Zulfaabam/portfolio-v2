5212d2a5b58c37e9d024fb2809404c08397d28eb
Zulfaabam <zulfafatahakbar@gmail.com>
2024-10-23 16:17:51 +0700

    move from CRA to Vite

---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { ThemeProvider } from './components/ThemeContext';
+
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <Router>
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
+    </Router>
+  </React.StrictMode>
+);
