a7f3087f5f3fc025abbc9925c043bf4a4c2e0457
Zulfaabam <zulfafatahakbar@gmail.com>
2024-11-08 19:43:49 +0700

    remove unused code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -20,21 +20,8 @@ export default {
       backgroundImage: {
         'earth-from-space': 'url(/earth-from-space.png)',
       },
-      // backgroundImage: {
-      //   'hero-dark': "url('svgs/Polygon-0.2s-1600px-2.svg')",
-      //   'hero-light': "url('svgs/Polygon-0.2s-1600px-2-light.svg')",
-      //   'btn-dark': "url('svgs/icon-sun.svg')",
-      //   'btn-light': "url('svgs/icon-moon.svg')",
-      //   'work-dark': "url('svgs/curve-line.svg')",
-      //   'work-light': "url('svgs/curve-line-light.svg')",
-      // },
+
       colors: {
-        // blue: '#0070f3',
-        // 'blue-light': '#00c6fb',
-        // cyan: '#50e3c2',
-        // dark: '#0F172A',
-        // 'dark-gray': '#1e2938',
-        // darker: '#0B1120',
         primary: '#76C1FF',
         secondary: '#133d9e',
         accent: '#00f9e9',
@@ -67,37 +54,6 @@ export default {
           },
         },
       },
-      // animation: {
-      //   wave: 'wave 2.5s infinite',
-      // },
-      // keyframes: {
-      //   wave: {
-      //     '0%': {
-      //       transform: 'rotate(0deg)',
-      //     },
-      //     '10%': {
-      //       transform: 'rotate(14deg)',
-      //     },
-      //     '20%': {
-      //       transform: ' rotate(-8deg)',
-      //     },
-      //     '30%': {
-      //       transform: 'rotate(14deg)',
-      //     },
-      //     '40%': {
-      //       transform: 'rotate(-4deg)',
-      //     },
-      //     '50%': {
-      //       transform: 'rotate(10deg)',
-      //     },
-      //     '60%': {
-      //       transform: 'rotate(0deg)',
-      //     },
-      //     '100%': {
-      //       transform: 'rotate(0deg)',
-      //     },
-      //   },
-      // },
     },
   },
   plugins: [addVariablesForColors],
