9eac6bf1ebc4ea0344917f71cb301d013efc5e19
Zulfaabam <zulfafatahakbar@gmail.com>
2024-11-09 16:34:19 +0700

    added floating navabr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,18 +2,20 @@ import About from '@/components/about';
 import FeaturedProjects from '@/components/featured-projects';
 import FloatingSocmed from '@/components/floating-socmed';
 import Hero from '@/components/hero';
+import Navbar from '@/components/navbar';
 import TellMeSection from '@/components/tell-me';
 
 export default function Homepage() {
   return (
     <div className='relative'>
+      <Navbar />
       <FloatingSocmed />
       <Hero />
       <div className='w-full bg-gradient-to-b from-dark via-dark to-black'>
         <About />
         <FeaturedProjects />
       </div>
-      <div className='bg-earth-from-space w-full bg-cover bg-no-repeat'>
+      <div className='w-full bg-earth-from-space bg-cover bg-no-repeat'>
         <TellMeSection />
       </div>
     </div>
