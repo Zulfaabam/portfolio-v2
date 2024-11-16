4358c9fd71d12b7ab079a7f148479f06fd3b81ab
Zulfaabam <zulfafatahakbar@gmail.com>
2024-11-16 14:46:22 +0700

    fix error build

--- src/lib/supabase/middleware.ts
+++ src/lib/supabase/middleware.ts
@@ -3,7 +3,7 @@ import { type NextRequest, NextResponse } from 'next/server';
 
 export const createClient = (request: NextRequest) => {
   // Create an unmodified response
-  let supabaseResponse = NextResponse.next({
+  const supabaseResponse = NextResponse.next({
     request: {
       headers: request.headers,
     },
